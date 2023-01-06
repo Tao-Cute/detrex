4961675d1d6efdca7bec65d84e157a4bbf460c03
Tao-Cute <475829184@qq.com>
2023-01-06 09:41:13 +0800

    add attn block

--- detrex/modeling/backbone/ViT.py
+++ detrex/modeling/backbone/ViT.py
@@ -29,27 +29,136 @@ from IPython import embed
 
 logger = logging.getLogger(__name__)
 
+class ConvMlp(nn.Module):
+    """ MLP using 1x1 convs that keeps spatial dims
+    """
+    def __init__(
+            self, in_features, hidden_features=None, out_features=None, act_layer=nn.ReLU,
+            norm_layer=None, bias=True, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        bias = to_2tuple(bias)
+
+        self.fc1 = nn.Conv2d(in_features, hidden_features, kernel_size=1, bias=bias[0])
+        self.norm = norm_layer(hidden_features) if norm_layer else nn.Identity()
+        self.act = act_layer()
+        self.drop = nn.Dropout(drop)
+        self.fc2 = nn.Conv2d(hidden_features, out_features, kernel_size=1, bias=bias[1])
+
+    def forward(self, x):
+        x = x.permute(2, 1, 0)
+        x = self.fc1(x)
+        x = self.norm(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = x.permute(1, 2 ,0)
+        return x
+
+class ChannelProcessing(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, attn_drop=0., 
+                    mlp_hidden_dim=None, act_layer=nn.GELU, drop=None, norm_layer=nn.LayerNorm, cha_sr_ratio=1, mlp="fc"):
+        super().__init__()
+        assert dim % num_heads == 0, f"dim {dim} should be divided by num_heads {num_heads}."
+
+        self.dim = dim
+        self.num_heads = num_heads
+        self.temperature = nn.Parameter(torch.ones(num_heads, 1, 1))
+
+        self.cha_sr_ratio = cha_sr_ratio if num_heads > 1 else 1
+        if mlp == "fc":
+            self.mlp_v = Mlp(in_features=dim//self.cha_sr_ratio, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        if mlp == "conv":
+            self.mlp_v = ConvMlp(in_features=dim//self.cha_sr_ratio, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        self.norm_v = norm_layer(dim//self.cha_sr_ratio)
+
+        self.q = nn.Linear(dim, dim, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+
+
+        self.apply(self._init_weights)
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+    def _gen_attn(self, q, k):
+        q = q.softmax(-2).transpose(-1,-2)
+        _, _, N, _  = k.shape
+        k = torch.nn.functional.adaptive_avg_pool2d(k.softmax(-2), (N, 1))
+        
+        attn = torch.sigmoid(q @ k)
+        return attn  * self.temperature
+    def forward(self, x, H, W):
+        B, N, C = x.shape
+        v = x.reshape(B, N, self.num_heads, C // self.num_heads // self.cha_sr_ratio).permute(0, 2, 1, 3)
+        q = self.q(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+        k = x.reshape(B, N, self.num_heads,  C // self.num_heads).permute(0, 2, 1, 3)
+
+        attn = self._gen_attn(q, k)
+        attn = self.attn_drop(attn)
+
+        Bv, Hd, Nv, Cv = v.shape
+        v = self.norm_v(self.mlp_v(v.transpose(1, 2).reshape(Bv, Nv, Hd*Cv))).reshape(Bv, Nv, Hd, Cv).transpose(1, 2)
+
+        repeat_time = N // attn.shape[-1]
+        attn = attn.repeat_interleave(repeat_time, dim=-1) if attn.shape[-1] > 1 else attn
+        x = (attn * v.transpose(-1, -2)).permute(0, 3, 1, 2).reshape(B, N, C)
+
+        return x,  attn * v.transpose(-1, -2) 
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'temperature'}
 
 class Block(nn.Module):
 
-    def __init__(
-            self, dim, num_heads, mlp_ratio=4., qkv_bias=False, drop=0., attn_drop=0., init_values=None,
-            drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm):
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, init_values=1e-4, channel_process="mlp", mlp="fc"):
         super().__init__()
         self.norm1 = norm_layer(dim)
         self.attn = Attention(dim, num_heads=num_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, proj_drop=drop)
-        self.ls1 = LayerScale(dim, init_values=init_values) if init_values else nn.Identity()
         # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
-        self.drop_path1 = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
         self.norm2 = norm_layer(dim)
-        self.mlp = Mlp(in_features=dim, hidden_features=int(dim * mlp_ratio), act_layer=act_layer, drop=drop)
-        self.ls2 = LayerScale(dim, init_values=init_values) if init_values else nn.Identity()
-        self.drop_path2 = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.gamma1 = nn.Parameter(init_values * torch.ones(dim), requires_grad=True)
+        self.gamma2 = nn.Parameter(init_values * torch.ones(dim), requires_grad=True)
+        if channel_process=="mlp":
+            self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        elif channel_process=="cp":
+            self.mlp = ChannelProcessing(dim, num_heads=num_heads, qkv_bias=qkv_bias, attn_drop=attn_drop, 
+                                        drop=drop, mlp_hidden_dim = int(dim * mlp_ratio), mlp=mlp)
+            self.H = None
+            self.W = None
 
     def forward(self, x):
-        x = x + self.drop_path1(self.ls1(self.attn(self.norm1(x))))
-        x = x + self.drop_path2(self.ls2(self.mlp(self.norm2(x))))
+        convert = False
+        if isinstance(x, torch.float16):
+            print("amp open")
+            x = x.float()
+            convert=True
+        if isinstance(self.mlp, Mlp):
+            x = x + self.drop_path(self.gamma1 * self.attn(self.norm1(x)))
+            x = x + self.drop_path(self.gamma2 * self.mlp(self.norm2(x)))
+        elif isinstance(self.mlp, ChannelProcessing):
+            H, W = self.H, self.W
+            x_new = self.attn(self.norm1(x))
+            x = x + self.drop_path(self.gamma1 * x_new)
+            x_new, _ = self.mlp(self.norm2(x), H, W)
+            x = x + self.drop_path(self.gamma2 * x_new)
+            self.H, self.W = H, W
+        if convert:
+            x = x.half()
         return x
 
 class BaseConv(nn.Module):
