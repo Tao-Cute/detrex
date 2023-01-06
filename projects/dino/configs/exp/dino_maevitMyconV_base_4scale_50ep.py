18aa5a31068dcbc5e4eaae8c557bfc5448c71301
root <475829184@qq.com>
2023-01-06 01:25:35 +0000

    add config

--- projects/dino/configs/exp/dino_maevitMyconV_base_4scale_50ep.py
+++ projects/dino/configs/exp/dino_maevitMyconV_base_4scale_50ep.py
@@ -8,5 +8,6 @@ from ..models.exp.dino_maevit_MyConv import model
 lr_multiplier = get_config("common/coco_schedule.py").lr_multiplier_50ep
 
 # modify training config
+train.max_iter = 375000
 train.init_checkpoint = "model_zoo/pretrained_model/mae_pretrain_vit_base.pth"
-train.output_dir = "./output/dino_maevitMyconv_base_4scale_12ep"
+train.output_dir = "./output/dino_maevitMyconv_base_4scale_50ep"
