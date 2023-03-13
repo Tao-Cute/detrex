332b79bd89174d8b8ff258f868761b0f1dfa515a
root <475829184@qq.com>
2023-03-13 03:59:32 +0000

    fix config

--- projects/ConvNextViT/configs/mask_rcnn_ConvNextViTDrop_3x.py
+++ projects/ConvNextViT/configs/mask_rcnn_ConvNextViTDrop_3x.py
@@ -5,13 +5,13 @@ from detrex.modeling.backbone import ConvNextWindowViT, ViTDrop
 from detectron2.modeling.backbone.fpn import LastLevelMaxPool
 
 model.backbone = L(FPN)(
-    bottom_up=L(ViTDrop)(),
+    bottom_up=L(ViTDrop)(drop_block=[0, 2]),
     in_features=["p0", "p1", "p2", "p3"],
     out_channels=256,
     top_block=L(LastLevelMaxPool)(),
 )
-train.init_checkpoint = "model_zoo/ViTDrop83.ckpt"
-train.output_dir = "./output/maskrcnn_ViTDrop3x"
+train.init_checkpoint = "model_zoo/Drop83.ckpt"
+train.output_dir = "./output/maskrcnn_ViTDrop2_2_3x"
 
 optimizer.lr = 0.0001
 optimizer.weight_decay = 0.05
--- projects/ConvNextViT/configs/mask_rcnn_ConvNextViT_1x.py
+++ projects/ConvNextViT/configs/mask_rcnn_ConvNextViT_1x.py
@@ -4,6 +4,9 @@ from detectron2.modeling.backbone import FPN
 from detrex.modeling.backbone import MyConvViT, MIMConvViT, ConvNextViT, ConvNextWindowViT
 from detectron2.modeling.backbone.fpn import LastLevelMaxPool
 
+from fvcore.common.param_scheduler import MultiStepParamScheduler
+from detectron2.solver import WarmupParamScheduler
+
 model.backbone = L(FPN)(
     bottom_up=L(ConvNextWindowViT)(),
     in_features=["p0", "p1", "p2", "p3"],
@@ -11,8 +14,18 @@ model.backbone = L(FPN)(
     top_block=L(LastLevelMaxPool)(),
 )
 train.init_checkpoint = "model_zoo/vit828.ckpt"
-train.output_dir = "./output/maskrcnn_convvit828exp3"
+train.output_dir = "./output/maskrcnn_convvit8281x"
 
 optimizer.lr = 0.0001
 optimizer.weight_decay = 0.05
 optimizer.params.overrides = {"pos_embed": {"weight_decay": 0.0}}
+
+train.max_iter = 90000
+lr_multiplier = L(WarmupParamScheduler)(
+    scheduler=L(MultiStepParamScheduler)(
+        values=[1.0, 0.1, 0.01],
+        milestones=[60000, 80000, 90000],
+    ),
+    warmup_length=500 / train.max_iter,
+    warmup_factor=0.001,
+)
