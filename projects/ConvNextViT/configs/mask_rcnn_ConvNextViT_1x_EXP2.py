8216725dd5bd3a6950d1a746ab045f82a520b7d2
root <495829184@qq.com>
2023-03-15 07:46:04 +0000

    push

--- projects/ConvNextViT/configs/mask_rcnn_ConvNextViT_1x_EXP2.py
+++ projects/ConvNextViT/configs/mask_rcnn_ConvNextViT_1x_EXP2.py
@@ -1,23 +1,22 @@
 from .mask_rcnn_r50_3x import model, dataloader, optimizer, lr_multiplier, train
 from detectron2.config import LazyCall as L
 from detectron2.modeling.backbone import FPN
-from detrex.modeling.backbone import ConvNextWindowViT, ViTDrop
+from detrex.modeling.backbone import MyConvViT, MIMConvViT, ConvNextViT, ConvNextWindowViT
 from detectron2.modeling.backbone.fpn import LastLevelMaxPool
 
 from fvcore.common.param_scheduler import MultiStepParamScheduler
 from detectron2.solver import WarmupParamScheduler
 
-
 model.backbone = L(FPN)(
-    bottom_up=L(ViTDrop)(drop_block=[0, 2]),
+    bottom_up=L(ConvNextWindowViT)(),
     in_features=["p0", "p1", "p2", "p3"],
     out_channels=256,
     top_block=L(LastLevelMaxPool)(),
 )
-train.init_checkpoint = "model_zoo/Drop83.ckpt"
-train.output_dir = "./output/maskrcnn_ViTDrop1x"
+train.init_checkpoint = "model_zoo/vit828.ckpt"
+train.output_dir = "./output/maskrcnn_convvit8281x_EXP2"
 
-optimizer.lr = 0.0002
+optimizer.lr = 0.0001
 optimizer.weight_decay = 0.1
 optimizer.params.overrides = {"pos_embed": {"weight_decay": 0.0}}
 
