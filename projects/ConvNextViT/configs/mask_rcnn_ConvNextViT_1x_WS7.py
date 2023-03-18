cb9d4b09e6557ccc169dacc87ea62ab3b7952361
Shiyi Lan <voidrank@gmail.com>
2023-03-17 23:41:11 -0700

    add

---
+++ projects/ConvNextViT/configs/mask_rcnn_ConvNextViT_1x_WS7.py
@@ -0,0 +1,31 @@
+from .mask_rcnn_r50_3x import model, dataloader, optimizer, lr_multiplier, train
+from detectron2.config import LazyCall as L
+from detectron2.modeling.backbone import FPN
+from detrex.modeling.backbone import MyConvViT, MIMConvViT, ConvNextViT, ConvNextWindowViT
+from detectron2.modeling.backbone.fpn import LastLevelMaxPool
+
+from fvcore.common.param_scheduler import MultiStepParamScheduler
+from detectron2.solver import WarmupParamScheduler
+
+model.backbone = L(FPN)(
+    bottom_up=L(ConvNextWindowViT)(drop_block=[0, 1, 2], window_size=7),
+    in_features=["p0", "p1", "p2", "p3"],
+    out_channels=256,
+    top_block=L(LastLevelMaxPool)(),
+)
+train.init_checkpoint = "model_zoo/ViTDrop.ckpt"
+train.output_dir = "./output/MaskRCNN_Drop03_1x_WS7"
+
+optimizer.lr = 0.0001
+optimizer.weight_decay = 0.1
+optimizer.params.overrides = {"pos_embed": {"weight_decay": 0.0}}
+
+train.max_iter = 90000
+lr_multiplier = L(WarmupParamScheduler)(
+    scheduler=L(MultiStepParamScheduler)(
+        values=[1.0, 0.1, 0.01],
+        milestones=[60000, 80000, 90000],
+    ),
+    warmup_length=500 / train.max_iter,
+    warmup_factor=0.001,
+)
